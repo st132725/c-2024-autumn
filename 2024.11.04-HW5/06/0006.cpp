0efb5c7b3c23a7bc786bc656ff91330cfd77cc47
unknown <st132725@student.spbu.ru>
2024-11-05 19:47:02 +0300

    123

--- 2024.11.04-HW5/06/0006.cpp
+++ 2024.11.04-HW5/06/0006.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 
+void sort(int arr[], int l) {
+	for (int i = 1; i < l; i++) {
+		int j = i - 1;
+		while (j >= 0 && *(arr + j) > *(arr + j + 1)) {
+			std::swap(*(arr + j), *(arr + j + 1));
+			j--;
+		}
+	}
+}
+
 int main(int argc, char* arg[]) {
 	int n = 0;
 	int m = 0;
@@ -13,20 +23,29 @@ int main(int argc, char* arg[]) {
 		scanf_s("%d", b + i);
 	}
 
-	int k = 0;
-	if (n >= m) {
-		k = n;
-	}
-	else {
-		k = m;
-	}
+	sort(a, n);
+	sort(b, m);
+	
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-
+			if (*(a + i) == *(b + j)) {
+				if ((*(a + i) != *(a + i + 1)) && (*(b + j) != *(b + j + 1)))  {
+					std::cout << *(a + i) << ' ';
+				}
+			}
 		}
 	}
-	
+
+	/*
+	for (int i = 0; i < n; ++i) {
+		std::cout << *(a + i) << ' ';
+	}
+	std::cout << "\n";
+	for (int i = 0; i < m; ++i) {
+		std::cout << *(b + i) << ' ';
+	}
+	*/
 
 	free(a);
 	free(b);
